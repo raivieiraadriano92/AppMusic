ed73c69cea574dbfa4594e58814acf50631f51b3
Raí Vieira Adriano <raivieiraadriano92@gmail.com>
2018-04-06 00:50:49 -0300

    redux/saga

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,281 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as AlbumsActions } from 'store/ducks/albums';
 
 import { FlatList, StatusBar, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import AlbumItem from './components/AlbumItem';
-
-import styles from './styles';
 import { colors } from 'styles';
+import AlbumItem from './components/AlbumItem';
+import styles from './styles';
 
-const albums = [
-  {
-    id: 0,
-    title: 'Hybrid Theory',
-    author: 'Linkin Park',
-    thumbnail: 'https://s3-sa-east-1.amazonaws.com/gonative/linkin_park.jpg',
-    songs: [
-      {
-        id: 0,
-        title: 'Papercut',
-        author: 'Linkin Park',
-      },
-      {
-        id: 1,
-        title: 'One Step Closer',
-        author: 'Linkin Park',
-      },
-      {
-        id: 2,
-        title: 'With You',
-        author: 'Linkin Park',
-      },
-      {
-        id: 3,
-        title: 'Points of Authority',
-        author: 'Linkin Park',
-      },
-      {
-        id: 4,
-        title: 'Crawling',
-        author: 'Linkin Park',
-      },
-    ]
-  },
-  {
-    id: 1,
-    title: 'Greatest Hits',
-    author: 'Foo Fighters',
-    thumbnail: 'https://s3-sa-east-1.amazonaws.com/gonative/foo_fighters.jpg',
-    songs: [
-      {
-        id: 0,
-        title: 'Papercut',
-        author: 'Linkin Park',
-      },
-      {
-        id: 1,
-        title: 'One Step Closer',
-        author: 'Linkin Park',
-      },
-      {
-        id: 2,
-        title: 'With You',
-        author: 'Linkin Park',
-      },
-      {
-        id: 3,
-        title: 'Points of Authority',
-        author: 'Linkin Park',
-      },
-      {
-        id: 4,
-        title: 'Crawling',
-        author: 'Linkin Park',
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: 'Live from Villa Mix',
-    author: 'Alok',
-    thumbnail: 'https://s3-sa-east-1.amazonaws.com/gonative/alok.jpg',
-    songs: [
-      {
-        id: 0,
-        title: 'Papercut',
-        author: 'Linkin Park',
-      },
-      {
-        id: 1,
-        title: 'One Step Closer',
-        author: 'Linkin Park',
-      },
-      {
-        id: 2,
-        title: 'With You',
-        author: 'Linkin Park',
-      },
-      {
-        id: 3,
-        title: 'Points of Authority',
-        author: 'Linkin Park',
-      },
-      {
-        id: 4,
-        title: 'Crawling',
-        author: 'Linkin Park',
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: 'Hybrid Theory',
-    author: 'Linkin Park',
-    thumbnail: 'https://s3-sa-east-1.amazonaws.com/gonative/linkin_park.jpg',
-    songs: [
-      {
-        id: 0,
-        title: 'Papercut',
-        author: 'Linkin Park',
-      },
-      {
-        id: 1,
-        title: 'One Step Closer',
-        author: 'Linkin Park',
-      },
-      {
-        id: 2,
-        title: 'With You',
-        author: 'Linkin Park',
-      },
-      {
-        id: 3,
-        title: 'Points of Authority',
-        author: 'Linkin Park',
-      },
-      {
-        id: 4,
-        title: 'Crawling',
-        author: 'Linkin Park',
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: 'Live from Villa Mix',
-    author: 'Alok',
-    thumbnail: 'https://s3-sa-east-1.amazonaws.com/gonative/alok.jpg',
-    songs: [
-      {
-        id: 0,
-        title: 'Papercut',
-        author: 'Linkin Park',
-      },
-      {
-        id: 1,
-        title: 'One Step Closer',
-        author: 'Linkin Park',
-      },
-      {
-        id: 2,
-        title: 'With You',
-        author: 'Linkin Park',
-      },
-      {
-        id: 3,
-        title: 'Points of Authority',
-        author: 'Linkin Park',
-      },
-      {
-        id: 4,
-        title: 'Crawling',
-        author: 'Linkin Park',
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: 'Greatest Hits',
-    author: 'Foo Fighters',
-    thumbnail: 'https://s3-sa-east-1.amazonaws.com/gonative/foo_fighters.jpg',
-    songs: [
-      {
-        id: 0,
-        title: 'Papercut',
-        author: 'Linkin Park',
-      },
-      {
-        id: 1,
-        title: 'One Step Closer',
-        author: 'Linkin Park',
-      },
-      {
-        id: 2,
-        title: 'With You',
-        author: 'Linkin Park',
-      },
-      {
-        id: 3,
-        title: 'Points of Authority',
-        author: 'Linkin Park',
-      },
-      {
-        id: 4,
-        title: 'Crawling',
-        author: 'Linkin Park',
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: 'Hybrid Theory',
-    author: 'Linkin Park',
-    thumbnail: 'https://s3-sa-east-1.amazonaws.com/gonative/linkin_park.jpg',
-    songs: [
-      {
-        id: 0,
-        title: 'Papercut',
-        author: 'Linkin Park',
-      },
-      {
-        id: 1,
-        title: 'One Step Closer',
-        author: 'Linkin Park',
-      },
-      {
-        id: 2,
-        title: 'With You',
-        author: 'Linkin Park',
-      },
-      {
-        id: 3,
-        title: 'Points of Authority',
-        author: 'Linkin Park',
-      },
-      {
-        id: 4,
-        title: 'Crawling',
-        author: 'Linkin Park',
-      },
-    ],
-  },
-];
+class Main extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Sua Biblioteca',
+    headerRight: (
+      <TouchableOpacity
+        onPress={() => { navigation.navigate('Search'); }}
+        style={styles.headerRight}
+      >
+        <Icon name="search" size={24} color={colors.white} />
+      </TouchableOpacity>
+    ),
+  });
 
-const Main = ({ navigation }) => (
-  <View style={styles.container}>
-    <StatusBar barStyle="light-content" />
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func.isRequired,
+    }).isRequired,
+    getAlbumsRequest: PropTypes.func.isRequired,
+    albums: PropTypes.shape({
+      data: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+      })).isRequired,
+    }).isRequired,
+  };
 
-    <FlatList
-      data={albums}
-      keyExtractor={album => String(album.id)}
-      renderItem={({ item }) => (
-        <AlbumItem
-          album={item}
-          onPress={() => navigation.navigate('Album', { album: item })}
+  componentDidMount() {
+    this.props.getAlbumsRequest();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+
+        <FlatList
+          data={this.props.albums.data}
+          keyExtractor={album => String(album.id)}
+          renderItem={({ item }) => (
+            <AlbumItem
+              album={item}
+              onPress={() => this.props.navigation.navigate('Album', { album: item })}
+            />
+          )}
         />
-      )}
-    />
-  </View>
-);
+      </View>
+    );
+  }
+}
 
-Main.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func.isRequired,
-  }).isRequired,
-};
-
-Main.navigationOptions = ({ navigation }) => ({
-  title: 'Sua Biblioteca',
-  headerRight: (
-    <TouchableOpacity
-      onPress={() => { navigation.navigate('Search'); }}
-      style={styles.headerRight}
-    >
-      <Icon name="search" size={24} color={colors.white} />
-    </TouchableOpacity>
-  ),
+const mapStateToProps = state => ({
+  albums: state.albums,
 });
 
-export default Main;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(AlbumsActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: 'http://192.168.124.1:3000',
 });
 
 export default api;
